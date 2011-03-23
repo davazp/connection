72bcb8f495659dfb0dd34f2d34e03d09a11fb527
David Vázquez <davazp@gmail.com>
2011-03-24 00:01:09 +0100

    Add a statusbar and ui_error and ui_message functions

--- src/conn.c
+++ src/conn.c
@@ -21,19 +21,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #include <gtk/gtk.h>
 #include <math.h>
 #include "conn-utils.h"
 #include "conn-hex.h"
 #include "conn-hex-widget.h"
 
+#define UI_BUILDER_FILENAME "./connection.ui"
 #define GET_OBJECT(obj) (GTK_WIDGET(gtk_builder_get_object (builder, (obj))))
 static GtkBuilder * builder;
-
 GtkWidget * hexboard;
 
+void
+ui_error (const gchar *fmt, ...)
+{
+  GtkWidget *window;
+  GtkWidget * dialog;
+  char buffer[256];
+  va_list va;
+  va_start (va,fmt);
+  vsnprintf (buffer, sizeof(buffer), fmt, va);
+  window = GET_OBJECT("window");
+  g_warning (buffer);
+  dialog = gtk_message_dialog_new (GTK_WINDOW(window),
+                                   GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                   GTK_MESSAGE_ERROR,
+                                   GTK_BUTTONS_OK,
+                                   buffer);
+  gtk_window_set_title (GTK_WINDOW (dialog), "Error");
+  gtk_dialog_run (GTK_DIALOG (dialog));
+  gtk_widget_destroy (dialog);
+  va_end (va);
+}
+
+static void
+ui_message (char * fmt, ...)
+{
+  char buffer[256];
+  va_list va;
+  static GtkWidget * statusbar;
+  static guint context;
+  static guint initialized = 0;
+  va_start (va, fmt);
+  if (!initialized)
+    {
+      statusbar = GET_OBJECT ("statusbar");
+      context = gtk_statusbar_get_context_id(GTK_STATUSBAR (statusbar), "Game messages");
+      initialized = 1;
+    }
+  vsnprintf (buffer, sizeof(buffer), fmt, va);
+  gtk_statusbar_pop (GTK_STATUSBAR (statusbar), context);
+  gtk_statusbar_push (GTK_STATUSBAR (statusbar), context, buffer);
+  va_end (va);
+}
+
+
+/* Signals */
+
+
 gboolean
-ui_about (GtkWidget * window, gpointer data)
+ui_signal_about (GtkWidget * window, gpointer data)
 {
   GtkWidget * dialog = GET_OBJECT ("about");
   gtk_dialog_run (GTK_DIALOG (dialog));
@@ -42,14 +90,15 @@ ui_about (GtkWidget * window, gpointer data)
 
 
 gboolean
-ui_quit (GtkWidget * window, gpointer data)
+ui_signal_quit (GtkWidget * window, gpointer data)
 {
   gtk_main_quit();
   return TRUE;
 }
 
+
 void
-ui_cell_clicked (GtkWidget * widget, gint i, gint j, hex_t game)
+ui_signal_cell_clicked (GtkWidget * widget, gint i, gint j, hex_t game)
 {
   double colors[3][3] = {{1,1,1}, {0,1,0}, {1,0,0}};
   int player;
@@ -57,17 +106,19 @@ ui_cell_clicked (GtkWidget * widget, gint i, gint j, hex_t game)
   player = hex_get_player (game);
   status = hex_move (game, i, j);
   if (status == HEX_SUCCESS)
-    hexboard_set_color (HEXBOARD(widget), i, j,
-                        colors[player][0],
-                        colors[player][1],
-                        colors[player][2]);
+    {
+      hexboard_set_color (HEXBOARD(widget), i, j,
+                          colors[player][0],
+                          colors[player][1],
+                          colors[player][2]);
+    }
   else
     gdk_beep();
 }
 
 
 gboolean
-ui_export (gpointer data)
+ui_signal_export (gpointer data)
 {
   GtkWidget *dialog;
   GtkWidget *window = GET_OBJECT("window");
@@ -105,7 +156,6 @@ ui_export (gpointer data)
       gint width, height;
       GtkFileFilter * filter;
       char * ext;
-
       filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
       filter = gtk_file_chooser_get_filter (GTK_FILE_CHOOSER (dialog));
       gdk_window_get_size (hexboard->window, &width, &height);
@@ -116,10 +166,6 @@ ui_export (gpointer data)
         ext = "png";
       else if (filter == filter_svg)
         ext = "svg";
-      else
-        conn_fatal ("Show messagebox error here.");
-
-      /* TODO: Error checking here. */
 
       g_free (filename);
     }
@@ -132,28 +178,24 @@ ui_export (gpointer data)
 int
 main (int argc, char * argv[])
 {
-  GtkWidget * window;
   GtkWidget * box;
   GtkWidget * about;
+  GtkWidget * window;
   hex_t game;
 
   gtk_init (&argc, &argv);
   builder = gtk_builder_new();
-  gtk_builder_add_from_file (builder, "./connection.ui", NULL);
+  gtk_builder_add_from_file (builder, UI_BUILDER_FILENAME, NULL);
   gtk_builder_connect_signals (builder, NULL);
-
   window = GET_OBJECT("window");
   box = GET_OBJECT("box");
   about = GET_OBJECT ("about");
 
   game = hex_new (13);
-
   gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (about), PACKAGE_VERSION);
   hexboard = hexboard_new();
-  g_signal_connect (GTK_WIDGET(hexboard), "cell_clicked", G_CALLBACK(ui_cell_clicked), game);
-
+  g_signal_connect (GTK_WIDGET(hexboard), "cell_clicked", G_CALLBACK(ui_signal_cell_clicked), game);
   gtk_container_add (GTK_CONTAINER(box), hexboard);
-
   gtk_widget_show_all (window);
   gtk_main();
   hex_free (game);
